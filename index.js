4a31eb0c29224630307c9d62d383f0149ae5e9cc
Doudou <dang.pascal@hotmail.fr>
2017-04-25 12:11:06 +0200

    MAJ Express

--- index.js
+++ index.js
@@ -1,82 +1,91 @@
-var http = require('http');
-var url = require('url');
-
-var server = http.createServer(function(req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "text/html"});
-    switch (page) {
-    	case '/' :
-        	res.write('<!DOCTYPE html>'+
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('<!DOCTYPE html>'+
 						'<html>'+
 						'    <head>'+
 						'        <meta charset="utf-8" />'+
-						'        <title>Bienvenue en cours de NodeJS</title>'+
+						'        <title>Coucou</title>'+
 						'    </head>'+ 
 						'    <body>'+
 						'     	<a href="contact.html">Lien vers Contact</a>'+
 						'     	<a href="todolist.html">Lien vers Todolist</a>'+
 						'     	<a href="login.html">Lien vers login</a>'+
 						'     	<a href="register.html">Lien vers Enregistrement</a>'+
+						'     	<p> TAMERE LA PUTE </p>'+
 						'    </body>'+
 						'</html>'
 					);
-        	break;
-        case '/contact.html' :
-        	res.write('<!DOCTYPE html>'+
-						'<html>'+
-						'    <head>'+
-						'        <meta charset="utf-8" />'+
-						'        <title>Bienvenue sur la page de Contact</title>'+
-						'    </head>'+ 
-						'    <body>'+
-						'     	<p> Bienvenue sur la page de contact </p>'+
-						'     	<a href="/">Retour</a>'+
-						'    </body>'+
-						'</html>');
-        	break;
-        case '/todolist.html' :
-        	res.write('<!DOCTYPE html>'+
-						'<html>'+
-						'    <head>'+
-						'        <meta charset="utf-8" />'+
-						'        <title>Bienvenue sur la page de Contact</title>'+
-						'    </head>'+ 
-						'    <body>'+
-						'     	<p> Bienvenue sur la page de la liste </p>'+
-						'     	<a href="/">Retour</a>'+
-						'    </body>'+
-						'</html>');
-        	break;
-        case '/login.html' :
-        	res.write('<!DOCTYPE html>'+
-						'<html>'+
-						'    <head>'+
-						'        <meta charset="utf-8" />'+
-						'        <title>Bienvenue sur la page de Contact</title>'+
-						'    </head>'+ 
-						'    <body>'+
-						'     	<p> Bienvenue sur la page de login </p>'+
-						'     	<a href="/">Retour</a>'+
-						'    </body>'+
-						'</html>');
-        	break;
-        case '/register.html' :
-        	res.write('<!DOCTYPE html>'+
-						'<html>'+
-						'    <head>'+
-						'        <meta charset="utf-8" />'+
-						'        <title>Bienvenue sur la page de Contact</title>'+
-						'    </head>'+ 
-						'    <body>'+
-						'     	<p> Bienvenue sur la page denregistrement </p>'+
-						'     	<a href="/">Retour</a>'+
-						'    </body>'+
-						'</html>');
-        	break;        	
-        default :
-        	res.writeHead(404, 'Erreur 404');
-        }
     res.end();
+    });
+
+app.get('/contact.html', function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.write('<!DOCTYPE html>'+
+		'<html>'+
+		'    <head>'+
+		'        <meta charset="utf-8" />'+
+		'        <title>Bienvenue sur la page de Contact</title>'+
+		'    </head>'+ 
+		'    <body>'+
+		'     	<p> Bienvenue sur la page de contact </p>'+
+		'     	<a href="/">Retour</a>'+
+		'    </body>'+
+		'</html>');
+	res.end();
+	});
+
+app.get('/todolist.html', function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.write('<!DOCTYPE html>'+
+		'<html>'+
+		'    <head>'+
+		'        <meta charset="utf-8" />'+
+		'        <title>Bienvenue sur la page de Contact</title>'+
+		'    </head>'+ 
+		'    <body>'+
+		'     	<p> Bienvenue sur la page de la liste </p>'+
+		'     	<a href="/">Retour</a>'+
+		'    </body>'+
+		'</html>');
+	res.end();
+	});
+
+app.get('/login.html', function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.write('<!DOCTYPE html>'+
+		'<html>'+
+		'    <head>'+
+		'        <meta charset="utf-8" />'+
+		'        <title>Bienvenue sur la page de Contact</title>'+
+		'    </head>'+ 
+		'    <body>'+
+		'     	<p> Bienvenue sur la page de login </p>'+
+		'     	<a href="/">Retour</a>'+
+		'    </body>'+
+		'</html>');
+	res.end();
+	});
+
+app.get('/register.html', function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.write('<!DOCTYPE html>'+
+		'<html>'+
+		'    <head>'+
+		'        <meta charset="utf-8" />'+
+		'        <title>Bienvenue sur la page de Contact</title>'+
+		'    </head>'+ 
+		'    <body>'+
+		'     	<p> Bienvenue sur la page denregistrement </p>'+
+		'     	<a href="/">Retour</a>'+
+		'    </body>'+
+		'</html>');
+	res.end();
+	});
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
 });
-server.listen(8080);
+app.listen(8080);

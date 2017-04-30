8a12ce388b40240cdf52ed6041a7e64452bab747
Doudou <dang.pascal@hotmail.fr>
2017-04-30 20:21:23 +0200

    Ajout des views EJS

--- index.js
+++ index.js
@@ -1,87 +1,28 @@
 var express = require('express');
 var app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('<!DOCTYPE html>'+
-						'<html>'+
-						'    <head>'+
-						'        <meta charset="utf-8" />'+
-						'        <title>Coucou</title>'+
-						'    </head>'+ 
-						'    <body>'+
-						'     	<a href="contact.html">Lien vers Contact</a>'+
-						'     	<a href="todolist.html">Lien vers Todolist</a>'+
-						'     	<a href="login.html">Lien vers login</a>'+
-						'     	<a href="register.html">Lien vers Enregistrement</a>'+
-						'     	<p> TAMERE LA PUTE </p>'+
-						'    </body>'+
-						'</html>'
-					);
-    res.end();
+    res.render('index');
     });
 
 app.get('/contact.html', function(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.write('<!DOCTYPE html>'+
-		'<html>'+
-		'    <head>'+
-		'        <meta charset="utf-8" />'+
-		'        <title>Bienvenue sur la page de Contact</title>'+
-		'    </head>'+ 
-		'    <body>'+
-		'     	<p> Bienvenue sur la page de contact </p>'+
-		'     	<a href="/">Retour</a>'+
-		'    </body>'+
-		'</html>');
-	res.end();
+	res.render('contact')
 	});
 
 app.get('/todolist.html', function(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.write('<!DOCTYPE html>'+
-		'<html>'+
-		'    <head>'+
-		'        <meta charset="utf-8" />'+
-		'        <title>Bienvenue sur la page de Contact</title>'+
-		'    </head>'+ 
-		'    <body>'+
-		'     	<p> Bienvenue sur la page de la liste </p>'+
-		'     	<a href="/">Retour</a>'+
-		'    </body>'+
-		'</html>');
-	res.end();
+	res.render('todolist')
 	});
 
 app.get('/login.html', function(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.write('<!DOCTYPE html>'+
-		'<html>'+
-		'    <head>'+
-		'        <meta charset="utf-8" />'+
-		'        <title>Bienvenue sur la page de Contact</title>'+
-		'    </head>'+ 
-		'    <body>'+
-		'     	<p> Bienvenue sur la page de login </p>'+
-		'     	<a href="/">Retour</a>'+
-		'    </body>'+
-		'</html>');
-	res.end();
+	res.render('login')
 	});
 
 app.get('/register.html', function(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.write('<!DOCTYPE html>'+
-		'<html>'+
-		'    <head>'+
-		'        <meta charset="utf-8" />'+
-		'        <title>Bienvenue sur la page de Contact</title>'+
-		'    </head>'+ 
-		'    <body>'+
-		'     	<p> Bienvenue sur la page denregistrement </p>'+
-		'     	<a href="/">Retour</a>'+
-		'    </body>'+
-		'</html>');
-	res.end();
+	res.render('register')
 	});
 
 app.use(function(req, res, next){
